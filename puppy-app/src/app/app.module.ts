4cbc9da554b504835fa9ee7cd374ce9d38053f40
unknown <cristianliljon@hotmail.com>
2019-09-27 16:00:16 -0500

    Tabla de perritos sin nombres solo imagenes

--- puppy-app/src/app/app.module.ts
+++ puppy-app/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PerrosComponent } from './perros/perros.component';
+import { PerroComponent } from './perro/perro.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    PerrosComponent,
+    PerroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ puppy-app/src/app/perro/perro.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Dog } from '../models/dogs';
+
+@Component({
+  selector: 'app-perro',
+  templateUrl: './perro.component.html',
+  styleUrls: ['./perro.component.scss']
+})
+export class PerroComponent implements OnInit {
+
+  @Input() dog:Dog;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
